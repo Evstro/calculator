e02b351cf459d1532d2bcc715c59a8b3cb65092d
Evstro <Evstro.code@gmail.com>
2022-08-10 15:52:39 -0500

    Buttons/Display Working for Single Calculations
    When a number or operator button is pressed, the display updates and the
    x, y, and operator values are set accordingly. Can run the Operate
    function fine once. Still need to work on being able to do operate
    multiple times in a row along with clearing all the variable on the clear
    button press
    
    [Ticket: X]

--- script.js
+++ script.js
@@ -2,53 +2,127 @@
 //get display area of calculator
 const display = document.getElementById('display');
 
-// const current = '';
-// let toAdd = '';
-
-
+//get clear button on calcultor
 const clearButton = document.getElementById('clears');
+
+function clearStrings(x, y, operator) {
+    x = '';
+    y = '';
+    operator = '';
+    return x, y, operator;
+}
+//add event listener that clears display when pressed
 clearButton.addEventListener('click', e => {
     e.preventDefault();
     display.innerText = '';
+    clearStrings(x, y, operator)
 })
 
+let x = '';
+let y = '';
+let operator = '';
 
-//get all buttons as a nodelist
-const buttons = document.querySelectorAll('.buttons');
+class NewValueInString {
+    constructor(buttonValue) {
+        this.buttonValue = buttonValue;
+    }
+    constructX(x) {
+        return x.concat(this.buttonValue)
+    }
+    constructY(y) {
+        return y.concat(this.buttonValue)
+    }
+    constructOperator(operator) {
+        return this.buttonValue
+    }
+}
 
+//get number buttons as a nodelist
+const numbbuttons = document.querySelectorAll('.numbbuttons');
 //add event listener on all buttons from nodelist
-for (const button of buttons) {
-    button.addEventListener('click', e => {
+for (const number of numbbuttons) {
+    number.addEventListener('click', e => {
         e.preventDefault();
-        let toAdd = button.value;
+        //variable to take the value of the button
+        let buttonValue = number.value;
+        //variable to get the current display text
         let current = display.innerText;
-        display.innerText = current.concat(toAdd);
+        setDisplay(current, buttonValue);
+        if (operator === '') {
+            x = new NewValueInString(buttonValue).constructX(x);
+        } else {
+            y = new NewValueInString(buttonValue).constructY(y);
+        }  
+        
+        console.log('x: ', x);
+        console.log('y: ', y);
+        console.log('operator: ', operator)
+        
+        // if (button.value = '+' || '-'|| '*' || '/') {
+        //     let operator = button.value;
+        //     console.log('operator: ', operator)
+        // }
+        
     })
 }
 
+//get operator buttons as a nodelist
+const opbuttons = document.querySelectorAll('.opbuttons');
+//add event listener on operator buttons from nodelist
+for (const opButton of opbuttons) {
+    opButton.addEventListener('click', e => {
+        e.preventDefault();
+        let buttonValue = opButton.value;
+        let current = display.innerText;
+        setDisplay(current, buttonValue);
+        operator = new NewValueInString(buttonValue).constructOperator(operator);
+        console.log('x: ', x);
+        console.log('y: ', y);
+        console.log('operator: ', operator)
+        return operator;
+    })
+}
+
+const equalsButton = document.getElementById('equals');
+
+equalsButton.addEventListener('click', e => {
+    e.preventDefault();
+    operate(operator, x, y);
+})
+
+//set display function
+function setDisplay(current, buttonValue) {
+
+    //sets html display to the previous variables with concat
+    display.innerText = current.concat(buttonValue);
+}
 
 // basic math functions
 function add(x, y) {
-    let sum = x + y;
+    let sum = Number(x) + Number(y);
     console.log(`${x} + ${y} = `, sum)
+    display.innerText = sum;
     return sum;
 }
 
 function subtract(x, y) {
     let difference = x - y;
     console.log(`${x} - ${y} = `, difference)
+    display.innerText = difference;
     return difference;
 }
 
 function multiply(x, y) {
     let product = x * y;
     console.log(`${x} * ${y} = `, product)
+    display.innerText = product;
     return product;
 }
 
 function divide(x, y) {
     let quotient = x / y;
     console.log(`${x} / ${y} = `, quotient)
+    display.innerText = quotient;
     return quotient;
 }
 
@@ -66,7 +140,3 @@ function operate(operator, x, y) {
     
 }
 
-operate('+', 5, 5);
-operate('-', 5, 5);
-operate('*', 5, 5);
-operate('/', 5, 5);
